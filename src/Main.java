11e24d95edd7a5344f251578fc7a94aa951b8ba9
Joshtg1104 <joshtg1104@gmail.com>
2019-05-13 12:18:52 -0500

    Okay

---
+++ src/Main.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        Exercise 1
+//        System.out.println("It's a great day today.");
+//        System.out.println("What is your name?");
+//        String name = scan.next();
+//        System.out.println("Hello " + name);
+
+//        Exercise 2
+//        System.out.println("Enter a number");
+//        Integer number1 = scan.nextInt();
+//        System.out.println("Enter another number");
+//        Integer number2 = scan.nextInt();
+//        System.out.println(number1 + "-" + number2 + "=" + (number1 - number2));
+
+//        Exercise 3
+//        System.out.println("What is your balance");
+//        Integer balance = scan.nextInt();
+//        System.out.println("I've granted you $1.50 more!");
+//        System.out.println("Your new balance is: " + "$" + (balance + 1.50));
+
+//        Exercise 4
+//        System.out.println("Enter password");
+//        String password1 = scan.next();
+//        System.out.println("Re-enter password for confirmation");
+//        String password2 = scan.next();
+//        if(password1.equals(password2)){
+//            System.out.println("Password Confirmed");
+//        }
+//        else{
+//            System.out.println("ERROR: Password Does Not Match");
+//        }
+
+//        Exercise 5
+//        System.out.println("Enter your answer");
+//        String number = scan.next();
+//        if(number.equals("1") || number.equals("2") || number.equals("3")){
+//            System.out.println("CORRECT");
+//        }
+//        else{
+//            System.out.println("ERROR");
+//        }
+
+//        Exercise 6
+//        System.out.println("Enter a number of your choice");
+//        Integer numbo = scan.nextInt();
+//        if(numbo > 0){
+//            System.out.println("POSITIVE");
+//        }
+//        else if(numbo < 0){
+//            System.out.println("NEGATIVE");
+//        }
+//        else if( numbo == 0){
+//            System.out.println("ZERO");
+//        }
+//        Exercise 7
+//        System.out.println("Give Me 3 Numbers");
+//        Integer number1 = scan.nextInt();
+//        Integer number2 = scan.nextInt();
+//        Integer number3 = scan.nextInt();
+//        Integer result = number1 + number2 + number3;
+//        if(result == 21){
+//            System.out.println("BLACKJACK!");
+//        }
+//        else if(result > 21){
+//            System.out.println("BUST!");
+//        }
+//        else{
+//            System.out.println("The total is: " + result);
+//        }
+
+//        Exercise 8
+//        System.out.println("Enter your number grade");
+//        Integer grade = scan.nextInt();
+//        if(grade > 100 || grade < 0){
+//            System.out.println("ERROR");
+//        }
+//        else if(grade <= 100 && grade >= 90){
+//            System.out.println("You have an A");
+//        }
+//        else if(grade <= 89 && grade >= 80){
+//            System.out.println("You have an B");
+//        }
+//        else if(grade <= 79 && grade >= 70){
+//            System.out.println("You have an C");
+//        }
+//        else if(grade <= 69 && grade >= 60){
+//            System.out.println("You have an D");
+//        }
+//        else if(grade <= 59 && grade >= 0){
+//            System.out.println("You have an F");
+//        }
+
+//        Exercise 9
+//        System.out.println("How tall are you in centimeters?");
+//        Integer height = scan.nextInt();
+//        System.out.println("You are " + (height / 30.48) + " in feet.");
+
+//        Exercise 10
+
+
+
+    }
+}
